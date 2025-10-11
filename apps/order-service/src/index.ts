11c4724978a9aba6379102621f630cd0c68bdc69
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-10-12 03:38:33 +0600

    Fastify order service

---
+++ apps/order-service/src/index.ts
@@ -0,0 +1,17 @@
+import Fastify from "fastify";
+
+const fastify = Fastify();
+
+/**
+ * Run the server! with async/await
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8001 });
+    console.log("Order service is running on port 8001");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
