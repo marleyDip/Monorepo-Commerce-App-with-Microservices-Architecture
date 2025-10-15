2f919c95f849833ee8304b15288da679d9bcd404
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-10-15 23:21:51 +0600

    Category API with Routes and Controllers

---
+++ apps/product-service/src/controllers/category.controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+
+export const createCategory = async (req: Request, res: Response) => {};
+export const updateCategory = async (req: Request, res: Response) => {};
+export const deleteCategory = async (req: Request, res: Response) => {};
+export const getCategories = async (req: Request, res: Response) => {};
